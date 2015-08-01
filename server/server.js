a883973444d88b4b966bd23bfca3f18263c0204b
Walker Flieller <wflieller@gmail.com>
2015-08-01 16:40:13 -0500

    Created background page and embedded resume

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+    Meteor.startup(function() {
+                Meteor.methods({
+                    'scroll': function() {
+                        $(".scroll").click(function(event) {
+                            event.preventDefault();
+                            $('html,body').animate({
+                                scrollTop: $('[name="' + this.hash.substring(1) + '"]').offset().top
+                            }, 500);
+                        });
+                    },
+                });
+            });
+        }
