2816117a5e026f91b37e9171916e39bb1b1069bf
Walker Flieller <wflieller@gmail.com>
2015-08-04 11:31:39 -0500

    Added Scrolling Routing Animation

--- client/routes.js
+++ client/routes.js
@@ -10,4 +10,25 @@ Router.map(function() {
 	this.route('portfolio');
 	// this.route('blog');
 
+	// this.route('drinkability', {
+	// 	path: '/portfolio#drinkability'
+
+	// });
+	// this.route('intro', {
+	// 	path: '/portfolio#intro'
+
+	// });
+
+	// this.route('appetite', {
+	// 	path: '/portfolio#appetite'
+	
+	// });
+
+	// this.route('tictactoe', {
+	// 	path: '/portfolio#tictactoe'
+	
+	// });
+IronRouterAutoscroll.animationDuration = 500;
+	
+
 });
